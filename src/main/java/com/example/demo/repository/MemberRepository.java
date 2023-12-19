33df5fc7deb815bf596c580a443d0d54ec6fc677
js1171 <bhd1171@naver.com>
2023-12-19 22:59:31 +0900

    :sparkles: MemberRepository 클래스에서 인터페이스로 수정

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MemberRepository {
-    public void save(Member member) {
-    }
+public interface MemberRepository extends JpaRepository<Member, String> {
+
 }
