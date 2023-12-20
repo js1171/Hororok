a046e7d6cb5b4cc19661a8531738127048245642
js1171 <bhd1171@naver.com>
2023-12-20 15:38:37 +0900

    :sparkles: DB연결 완료

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -3,8 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    boolean existsById(String id);
 }
