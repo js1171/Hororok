640b956c75105f5efcc8a1b0db57804d80488fb4
js1171 <bhd1171@naver.com>
2023-12-21 21:17:02 +0900

    :bug: Comment 생성 구현

--- src/main/java/com/example/demo/entity/Comment.java
+++ src/main/java/com/example/demo/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import com.example.demo.dto.member.request.CommentDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comment")
@@ -18,17 +20,29 @@ public class Comment {
 
     @Id
     @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
 
+    @Column(name = "feed_id")
     private Long feedId;
+
+    @Column(name = "user_id")
     private Long userId;
+
     private String contents;
 
     @CreatedDate
-    @Column(updatable = false, nullable = false)
-    private LocalDate createdAt;
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDate updatedAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Comment (CommentDTO dto, Long feedId, Long userId) {
+        this.feedId = feedId;
+        this.userId = userId;
+        this.contents = dto.getContents();
+    }
 }
