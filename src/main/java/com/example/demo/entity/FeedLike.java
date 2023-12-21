e2482f06e22c8b94b7866880b93963054080c5e1
HyeJiRoh <shgpwl509@gmail.com>
2023-12-21 23:37:44 +0900

    :sparkles: FeedLike 기능 추가

---
+++ src/main/java/com/example/demo/entity/FeedLike.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "likes")
+public class FeedLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member member;
+
+    public FeedLike(Member member, Feed feed) {
+        this.member = member;
+        this.feed = feed;
+    }
+}
