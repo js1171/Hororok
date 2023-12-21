0cc5c6c279694da397c7ba297db079e41a05caef
js1171 <bhd1171@naver.com>
2023-12-21 11:38:10 +0900

    :sparkles: 유저 (한명, 여러명) 조회 기능 구현

--- src/main/java/com/example/demo/controller/FollowController.java
+++ src/main/java/com/example/demo/controller/FollowController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Follow;
 import com.example.demo.repository.FollowRepository;
 import com.example.demo.service.FollowService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
@@ -11,6 +17,11 @@ public class FollowController {
 
     FollowRepository followRepository;
     private final FollowService followService;
+
+//    @PostMapping("/users/{fromUserId}/follows/{toUserId}")
+//    public ResponseEntity<String> follow(@RequestBody Map<Long, Long> params) {
+//        Follow follow = followRepository.findById(params.get("fromUserId"), )
+//    }
 
 
 
