01f588884f1ce978373bab8c3bed43fe0fe9a7f1
HyeJiRoh <shgpwl509@gmail.com>
2023-12-22 00:48:44 +0900

    :fire: 불필요한 member package 제거 및 import 변경

--- src/main/java/com/example/demo/HororokApplication.java
+++ src/main/java/com/example/demo/HororokApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.service.member.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
