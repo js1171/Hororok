c81468f2afadd5f341f2dd286dd5addddc8921ca
HyeJiRoh <shgpwl509@gmail.com>
2023-12-21 14:45:28 +0900

    :sparkles: Feed Entity 추가

---
+++ src/main/java/com/example/demo/entity/Feed.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import com.example.demo.dto.member.request.FeedRequestDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "feed")
+@NoArgsConstructor
+public class Feed {
+    @Id
+    @Column(name = "feed_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedId;
+
+    @Column(name = "contents")
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member member;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Feed(FeedRequestDTO feedRequestDTO, Member member) {
+        this.contents = feedRequestDTO.getContents();
+        this.member = member;
+        this.createdAt = LocalDateTime.now();
+    }
+}
