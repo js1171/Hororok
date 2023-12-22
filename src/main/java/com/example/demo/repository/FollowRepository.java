0955b68fa59d173084286aa1597c7954ce73507e
HyeJiRoh <shgpwl509@gmail.com>
2023-12-22 14:56:42 +0900

    :fire: FollowRepository 불필요한 import 제거

--- src/main/java/com/example/demo/repository/FollowRepository.java
+++ src/main/java/com/example/demo/repository/FollowRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
 }
