51e5ba2dcdf04123db76efe273267744164faffc
HyeJiRoh <shgpwl509@gmail.com>
2023-12-22 22:08:18 +0900

    :zap:  MemberResponse에서 MemberResponseDTO로 이름 수정

--- src/main/java/com/example/demo/dto/member/response/MemberResponseDTO.java
+++ src/main/java/com/example/demo/dto/member/response/MemberResponseDTO.java
@@ -1,40 +1,32 @@
 package com.example.demo.dto.member.response;
 
 import com.example.demo.entity.Member;
-import jakarta.persistence.Column;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor(access= AccessLevel.PROTECTED)
 @Getter
-public class MemberResponse {
+public class MemberResponseDTO {
     private Long userId;
     private String id;
-    private String password;
     private String name;
     private String nickname;
     private LocalDate birth;
     private char gender;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    public MemberResponse(Member member) {
+    public MemberResponseDTO(Member member) {
         this.userId = member.getUserId();
         this.id = member.getId();
-        this.password = member.getPassword();
         this.name = member.getName();
         this.nickname = member.getNickname();
         this.birth = member.getBirth();
         this.gender = member.getGender();
-        this.createdAt = member.getCreatedAt();
-        this.updatedAt = member.getUpdatedAt();
     }
 
-    public MemberResponse(long userId, String id) {
+    public MemberResponseDTO(long userId, String id) {
         this.userId = userId;
         this.id = id;
     }
