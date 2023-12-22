6d45de48ea1c05ceb82dedb7b12b89e0e5f866a5
HyeJiRoh <shgpwl509@gmail.com>
2023-12-22 14:56:23 +0900

    :fire: FollowController followUser 기능 추가

--- src/main/java/com/example/demo/controller/FollowController.java
+++ src/main/java/com/example/demo/controller/FollowController.java
@@ -3,28 +3,37 @@ package com.example.demo.controller;
 import com.example.demo.entity.Follow;
 import com.example.demo.repository.FollowRepository;
 import com.example.demo.service.FollowService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class FollowController {
 
-    FollowRepository followRepository;
     private final FollowService followService;
+    private final HttpSession httpSession;
 
-//    @PostMapping("/users/{fromUserId}/follows/{toUserId}")
-//    public ResponseEntity<String> follow(@RequestBody Map<Long, Long> params) {
-//        Follow follow = followRepository.findById(params.get("fromUserId"), )
-//    }
+    @PostMapping("/users/{userId}/follows/{toUserId}")
+    public ResponseEntity<Void> followUser(
+            @PathVariable("userId") Long userId,
+            @PathVariable("toUserId") Long toUserId) {
+        Long fromUserId = (Long) httpSession.getAttribute("userId");
+        if (fromUserId == null || !fromUserId.equals(userId)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User가 로그인되어 있지 않거나 권한이 없습니다.");
+        }
 
+        followService.follow(fromUserId, toUserId);
 
-
-
-
+        return ResponseEntity.noContent().build();
+    }
 }
