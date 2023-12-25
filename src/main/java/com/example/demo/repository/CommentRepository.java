ca5249af631cd849c57bfd73987165c711c929cd
js1171 <bhd1171@naver.com>
2023-12-25 15:18:13 +0900

    :zap: 쿠키저장빼고 완료

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByFeedId(Long feedId);
